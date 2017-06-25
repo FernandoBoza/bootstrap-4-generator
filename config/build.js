980ceddbe07067337778cb6f06b4d325a20b17ec
Fernando Boza <hi@fernandoboza.com>
2017-06-25 14:06:41 -0400

    publish to npm

--- config/build.js
+++ config/build.js
@@ -1,46 +1,46 @@
 const fs = require('fs-extra')
 
-fs.copy('./config/src/css', './css', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/css', './css', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/js', './js', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/js', './js', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/scss', './scss', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/scss', './scss', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/.babelrc', './.babelrc', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/.babelrc', './.babelrc', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/.gitignore', './.gitignore', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/.gitignore', './.gitignore', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/gulpfile.js', './gulpfile.js', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/gulpfile.js', './gulpfile.js', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/index.html', './index.html', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/index.html', './index.html', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/package.json', './package.json', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/package.json', './package.json', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
 
-fs.copy('./config/src/README.md', './README.md', err => {
+fs.copy('./node_modules/bootstrap-4-generator/config/src/README.md', './README.md', err => {
   if (err) return console.error(err)
   console.log('success!')
 }) 
