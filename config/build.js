17d7b99c2a7cd05cb62fdc1a29732650d7dba16a
Fernando Boza <hi@fernandoboza.com>
2017-06-25 13:12:26 -0400

    almost done with node script

--- config/build.js
+++ config/build.js
@@ -1,7 +1,47 @@
 const fs = require('fs-extra')
 
-fs.copy('./css/', '../css', err => {
+fs.copy('./config/src/css', './css', err => {
   if (err) return console.error(err)
-
   console.log('success!')
-})
+}) 
+
+fs.copy('./config/src/js', './js', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
+fs.copy('./config/src/scss', './scss', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
+fs.copy('./config/src/.babelrc', './.babelrc', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
+fs.copy('./config/src/.gitignore', './.gitignore', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
+fs.copy('./config/src/gulpfile.js', './gulpfile.js', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
+fs.copy('./config/src/index.html', './index.html', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
+fs.copy('./config/src/package.json', './package.json', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
+fs.copy('./config/src/README.md', './README.md', err => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) 
+
